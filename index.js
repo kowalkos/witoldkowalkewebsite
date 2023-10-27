0508f1b49c5d4465728742af91672f6af6fbfce3
witoldkowalke <witoldkowalke@pop-os.localdomain>
2023-10-27 17:39:48 +0200

    added bootstrap and checked vim possibilities

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express'); //Import the express dependency
+const path = require('path');       //Import the path dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
 
 //Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
-});
+app.use(express.static(path.join(__dirname,'public')));  //serve static files from the public folder
 
 app.get('/about', (req, res) => {   //get requests to "/about" will route here
     res.sendFile('pages/aboutMe.html', {root: __dirname});      //server responds by sending the about.html file to the client's browser
